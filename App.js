e65ca7c180d6efebca69673ee77737b3d0605a82
Lawrence <lhshein14@gmail.com>
2024-01-06 17:12:19 +0800

    flexbox

--- App.js
+++ App.js
@@ -3,26 +3,13 @@ import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 import {useDimensions, useDeviceOrientation} from "@react-native-community/hooks"
 
 export default function App() {
-  const o = useDeviceOrientation()
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: "blue",
-        width:"100%",
-        height: o==='landscape'? "100%": "30%",
-      }}>
-      </View>
-      
-    </SafeAreaView>
-  );
+  return(
+    <View style={{
+      backgroundColor:"blue"
+    }}>
+    </View>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: "top",
-    
-  },
-});
+
