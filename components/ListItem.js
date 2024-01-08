347f48e77f83ccc8911dacf2335aac3ceb2f3a66
Lawrence <lhshein14@gmail.com>
2024-01-08 19:05:44 +0800

    renderSwipe

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableHighlight } from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+import {Swipeable, GestureHandlerRootView} from 'react-native-gesture-handler';
 function ListItem({name, listings, image, onPress, renderRight}) {
     return (
-        <Swipeable renderRightActions={renderRight}>
-            <TouchableHighlight underlayColor={"lightgrey"} onPress={onPress}>
-                <View style={styles.listContainer}>
-                    <Image style={styles.image} source={{uri:image}}/>
-                    <View style={styles.nameContainer}>
-                        <Text style={styles.name}>{name}</Text>
-                        <Text style={styles.listings}>{listings}</Text>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Swipeable renderRightActions={renderRight}>
+                <TouchableHighlight underlayColor={"lightgrey"} onPress={onPress}>
+                    <View style={styles.listContainer}>
+                        <Image style={styles.image} source={{uri:image}}/>
+                        <View style={styles.nameContainer}>
+                            <Text style={styles.name}>{name}</Text>
+                            <Text style={styles.listings}>{listings}</Text>
+                        </View>
                     </View>
-                </View>
-            </TouchableHighlight>
-        </Swipeable>
+                </TouchableHighlight>
+            </Swipeable>
+        </GestureHandlerRootView>
+
+        
     )
 }
 const styles = StyleSheet.create({
